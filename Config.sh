64f73e370b46e422d978140cd07febfdc3ecebb7
Abhishek Banthia <abanthia@apple.com>
2016-10-24 13:31:40 -0700

    Shell script.

---
+++ Config.sh
@@ -0,0 +1,3 @@
+# Retraining part (You need to be in the root TF folder. You can download TF from here: https://github.com/tensorflow/tensorflow)
+
+python tensorflow/examples/image_retraining/retrain.py --image_dir ~/tf_files/ --bottleneck_dir=~/tf_files/bottlenecks --how_many_training_steps 500 --model_dir=~/tf_files/inception --output_graph=~/tf_files/retrained_graph.pb --output_labels=~/tf_files/retrained_labels.txt/Users/abhishek/Downloads/tensorflow/\~/tf_files/retrained_labels.txt 
